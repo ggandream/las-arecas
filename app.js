3b179a0f20984cbd36372c8743282d20f8140426
Andrea <ggandream24@gmail.com>
2026-08-15 21:19:38 -0600

    feat: mobile version

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const btnMobileMenu = document.querySelector('[data-id="menu-btn"]');
+const mobileMenu = document.querySelector("#mobile-nav");
 
 btnMobileMenu.addEventListener("click", () => {
-  console.log("click al menu");
+  const isOpen = mobileMenu.getAttribute("data-show") === "true";
+  mobileMenu.setAttribute("data-show", String(!isOpen));
+  btnMobileMenu.setAttribute("aria-expanded", String(!isOpen));
+  btnMobileMenu.firstElementChild.classList.toggle("hidden");
+  btnMobileMenu.lastElementChild.classList.toggle("hidden");
 });
